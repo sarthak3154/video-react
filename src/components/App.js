41cb95fec70b6435ec732d4bae5619f67780b3b0
sarthak3154 <sarthak.arora3154@gmail.com>
2018-12-16 12:34:34 -0500

    video list rendering and detail

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
 
-    state = {videos: []}
+    state = {videos: [], selectedVideo: null};
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    };
 
     onSearchSubmit = async (term) => {
         const response = await youtube.get('/search', {
@@ -12,13 +18,26 @@ class App extends React.Component {
                 q: term
             }
         });
-        this.setState({videos: response.data.items})
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
         return true;
     };
 
     render() {
         return (
-            <div><SearchBar onSubmit={this.onSearchSubmit}/></div>
+            <div className="ui container">
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+                <div className="ui grid">
+                    <div className="eleven wide column">
+                        <VideoDetail video={this.state.selectedVideo}/>
+                    </div>
+                    <div className="five wide column">
+                        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
